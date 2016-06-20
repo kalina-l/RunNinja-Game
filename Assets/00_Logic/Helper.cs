00685c86ce13e76591430240f243963563be12b6
fabiberlin <fabi.in.berlin@googlemail.com>
2016-06-21 01:19:54 +0200

    idToColor - how to get the shader?

---
+++ Assets/00_Logic/Helper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helper : MonoBehaviour {
+
+    static Color[] colors = { Color.white, Color.blue, Color.red, Color.green, Color.yellow, Color.black };
+
+    public static Color idToColor(int id)
+    {
+        return colors[id];
+    }
+}
