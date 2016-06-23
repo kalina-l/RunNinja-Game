471db6bda7e6a7a7d9b382e097b6e13c397a2401
FrankBastian <frank.bastian87@googlemail.com>
2016-06-23 15:14:40 +0200

    sword attacks can Destroy crates

---
+++ Assets/00_Logic/SwordCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordCollider : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Obstacle")
+        {
+            other.GetComponent<Obstacle>().Destroy();
+        }
+    }
+}
